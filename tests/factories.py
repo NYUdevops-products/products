cbc14de7baa21421396cc3a954b4e38c446e0ad1
mt4456 <mt4456@nyu.com>
2021-10-16 09:50:35 -0400

    add product model detail and part of factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,5 +1,7 @@
 import factory
 from factory.fuzzy import FuzzyChoice
+from service.models import Product
 
 class ProductFactory(factory.Factory):
+    create a product(name="iPhone", category="phone", amount=1, description="the latest iPhone 13", status="Status.Good")
     
