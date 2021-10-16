62acb34e98139239a64671f1b7d56026d3600e06
mt4456 <mt4456@nyu.com>
2021-10-16 10:24:34 -0400

    edit factories.py and test_models.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,7 +1,13 @@
 import factory
 from factory.fuzzy import FuzzyChoice
-from service.models import Product
+from service.models import Product, Status
 
 class ProductFactory(factory.Factory):
-    create a product(name="iPhone", category="phone", amount=1, description="the latest iPhone 13", status="Status.Good")
-    
+    class Meta:
+        model = Product
+
+    id = factory.Sequence(lambda n:n)
+    name = factory.Faker("random_name")
+    category = FuzzyChoice(choices = ['phone', 'table', 'computer', 'curtain'])
+    amount = FuzzyChoice(choices = [1,2,3,4,5,6,7,8,9,10])
+    status = FuzzyChoice(choices = [Status.Good, Status.Normal, Status.Bad, Status.Unknow])    
