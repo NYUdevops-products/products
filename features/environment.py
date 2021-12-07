8ea2709b4ae678e79b2196f020198603c396c32a
zihaowang0916 <zihaowang0916@gmail.com>
2021-12-07 12:31:50 -0500

    remove route in manifest and replace BASEURL in environment

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,11 @@ from os import getenv
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
+# BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
+
+BASE_URL = getenv('BASE_URL', 'https://nyu-product-service-fall2021.us-south.cf.appdomain.cloud/')
+
+# https://nyu-product-service-fall2021.us-south.cf.appdomain.cloud/
 
 def before_all(context):
     """ Executed once before all tests """
